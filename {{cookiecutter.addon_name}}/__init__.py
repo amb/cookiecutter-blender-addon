99c2ded2936260f7d7481515cbbb201e5d34fa25
Tommi Hyppänen <thyppane@gmail.com>
2020-10-18 13:10:49 +0300

    Restructurinc

---
+++ {{cookiecutter.addon_name}}/__init__.py
@@ -0,0 +1,26 @@
+bl_info = {
+    "name": "{{cookiecutter.addon_name}}",
+    "author": "{{cookiecutter.author}}",
+    "version": ({{cookiecutter.version.split(".") | join(", ")}}),
+    "blender": ({{cookiecutter.blender_version.split(".") | join(", ")}}),
+    "location": "",
+    "description": "{{cookiecutter.description}}",
+    "warning": "",
+    "wiki_url": "",
+    "support": "COMMUNITY",
+    "category": "{{cookiecutter.category}}",
+}
+
+__version__ = ".".join(map(str, bl_info["version"]))
+
+import bpy
+import numpy as np
+import mathutils as mu
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
